dddef07109fbe95442d75316e8a63ce9475adde4
Kota Kawagoe <s2122126@cco.kanagawa-it.ac.jp>
2025-04-27 20:43:57 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import  render_template, redirect, session, request, Flask, url_for, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#デザイン画面
+@app.route('/announce')
+def announce():
+    return render_template('announce.html')
+
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+#アプリの実行する
+if __name__ == '__main__':
+    app.run(debug=True)
